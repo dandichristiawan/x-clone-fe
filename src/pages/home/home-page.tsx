6ec7279551e1dddc3a0048448ddace7579d779ff
DANDI CHRISTIAWAN <dandichristiawan@gmail.com>
2024-07-23 22:30:41 +0700

    add get all tweets/post fetch logic

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import { MainLayout } from '@/layout/main-layout'
+import React from 'react';
+import { MainLayout } from '@/layout/main-layout';
 
 interface PropsData {
-    _id: number,
-    username: string
-    email: string
+  _id: string;
+  user: {
+    _id: string;
+    username: string;
+  };
+  content: string;
+  likes: number;
+  createdAt: string;
 }
 
 export const HomePage = () => {
-    const [uname, setUname] = React.useState<PropsData[]>()
+  const [data, setData] = React.useState<PropsData[]>();
 
-    React.useEffect(() => {
-        async function get() {
-            try {
-                const res = await fetch('http://192.168.1.153:3000/api/users', {
-                    mode: 'cors',
-                    method: 'GET',
-                    headers: {'Content-Type':'application/json'},
-                })
-                const data = await res.json()
-                setUname(data)
-                console.log(data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        get()
-    }, [])
+  React.useEffect(() => {
+    async function get() {
+      try {
+        const res = await fetch('http://192.168.1.153:3000/api/getPosts', {
+          mode: 'cors',
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+        const data = await res.json();
+        setData(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    get();
+  }, []);
 
-    return (
-        <>
-            <MainLayout>
-                <main className="flex-1 mx-64 overflow-auto min-h-screen">{uname?.map((i) => (
-                    <div className="">{i.username}</div>
-                ))}</main>
-            </MainLayout>
-        </>
-    )
-}
+  return (
+    <>
+      <MainLayout>
+        <main className="flex-1 mx-64 overflow-auto min-h-screen">
+          {data?.map((i) => (
+            <div className="flex flex-col border border-l-0 border-r-0 border-b-0 border-red-600">
+              <div className="flex flex-row">
+                <div className="rounded-full bg-gray-500 p-2 w-10">a</div>
+                <div className="flex flex-col">
+                  <h1 className="my-2">{i.user.username}</h1>
+                  <p>{i.content}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </main>
+      </MainLayout>
+    </>
+  );
+};
